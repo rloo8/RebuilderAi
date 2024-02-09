cf18817cf5bb473387f8f4bf75da13d266f19441
rloo8 <cc1647@naver.com>
2024-02-10 03:41:56 +0900

    Feat: Header pc/mobile 컴포넌트 분리 및 미디어쿼리 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import Header from "./Components/HeaderComponent";
+import styled from "styled-components";
+import HeaderMobile from "./Components/HeaderMobile";
+import HeaderPc from "./Components/HeaderPc";
+
+const StyledHeader = styled.div`
+  @media (max-width: 768px) {
+    .pc-header {
+      display: none;
+    }
+  }
+
+  @media (min-width: 769px) {
+    .mobile-header {
+      display: none;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <StyledHeader>
+        <HeaderPc className="pc-header" />
+        <HeaderMobile className="mobile-header" />
+      </StyledHeader>
+      <video src="/image/main_video.mp4"></video>
+      <video src="/image/main_video.mp4"></video>
     </div>
   );
 }
